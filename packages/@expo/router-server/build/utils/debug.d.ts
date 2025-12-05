688eda3c7c0f4ad109489e46d5cd04b2a4869717
Hassan Khan <hassan@expo.dev>
2025-12-05 14:22:13 +0000

    chore: commit build outputs

---
+++ packages/@expo/router-server/build/utils/debug.d.ts
@@ -0,0 +1,16 @@
+/**
+ * Copyright © 2025 650 Industries.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+/**
+ * Creates a debug logger that is excluded from production bundles.
+ *
+ * In development, it uses the `debug` package for namespaced logging.
+ *
+ * In production, it returns a no-op function to avoid bundling `debug`. This is useful for SSR,
+ * where we bundle `@expo/router-server/node/render.js`.
+ */
+export declare function createDebug(namespace: string): (...args: unknown[]) => void;
+//# sourceMappingURL=debug.d.ts.map
---
+++ packages/@expo/router-server/build/utils/debug.js
@@ -0,0 +1,24 @@
+"use strict";
+/**
+ * Copyright © 2025 650 Industries.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.createDebug = createDebug;
+/**
+ * Creates a debug logger that is excluded from production bundles.
+ *
+ * In development, it uses the `debug` package for namespaced logging.
+ *
+ * In production, it returns a no-op function to avoid bundling `debug`. This is useful for SSR,
+ * where we bundle `@expo/router-server/node/render.js`.
+ */
+function createDebug(namespace) {
+    if (__DEV__) {
+        return require('debug')(namespace);
+    }
+    return () => { };
+}
+//# sourceMappingURL=debug.js.map
